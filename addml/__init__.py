ef2f9f8c1341a846fcec1b4a3ad9b36e87254aa6
Vili Oja <vili.oja@csc.fi>
2021-12-13 11:42:21 +0200

    KDKPAS-2718 Ignore flake8 errors in init.py
    Flake8 doesn't like the imports in __init__.py, but they are required
    for the way e.g. siptools currently uses addml.

--- addml/__init__.py
+++ addml/__init__.py
@@ -1,7 +1,7 @@
 """Import basic library files for making convenient use of the library
 possible.
 """
-
-from addml.base import *
-from addml.flatfiles import *
-from addml.split_addml import *
+# flake8 doesn't like these imports, but they are needed for other repos
+from addml.base import *  # noqa: F401,F403
+from addml.flatfiles import *  # noqa: F401,F403
+from addml.split_addml import *  # noqa: F401,F403
