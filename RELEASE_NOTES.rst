44a19aee8add077a151d346e6c07c482af34705f
Johan Kylander <johan.kylander@csc.fi>
2023-03-30 07:46:11 +0300

    KDKPAS-2987 Bump version and remove python2 support officially

---
+++ RELEASE_NOTES.rst
@@ -0,0 +1,8 @@
+Release notes
+=============
+
+Version 0.11
+------------
+
+- Python 2.7 support officially removed
+
--- addml/__init__.py
+++ addml/__init__.py
@@ -1,6 +1,8 @@
 """Import basic library files for making convenient use of the library
 possible.
 """
+__version__ = '0.11'
+
 # flake8 doesn't like these imports, but they are needed for other repos
 from addml.base import *  # noqa: F401,F403
 from addml.flatfiles import *  # noqa: F401,F403
